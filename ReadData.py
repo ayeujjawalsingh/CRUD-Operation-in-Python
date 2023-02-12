a3a7177c3bab1176c08ff96a0c45ebf37bbfebc3
Ujjawal Singh <ayeujjawalsingh@gmail.com>
2023-02-12 21:13:34 +0530

    Compelete

--- ReadData.py
+++ ReadData.py
@@ -1,20 +1,20 @@
 import CreateConnection
 def read():
-    row = int(input("How much number of row you want to print : "))
-    Query1 = "Select Id,Name,Mobile,Email,Address,City,State,PinCode,DateOfBirth From user_details"
-    # Query1 = "SELECT * FROM user_details LIMIT 30"
-    # Query1 = "SELECT * FROM user_details LIMIT 10 OFFSET 15"
-    # Query1 = "SELECT * FROM user_details LIMIT 15, 10"
+    # row = int(input("How much number of row you want to print : "))
+    # Query1 = "Select id,first_name,last_name,mobile_number,email_id,address,city,state,country,pin_code,date_of_birth From user_details Where status = 1"
+    # Query1 = "SELECT id,first_name,last_name,mobile_number,email_id,address,city,state,country,pin_code,date_of_birth FROM user_details order by id LIMIT 30"
+    Query1 = "SELECT id,first_name,last_name,mobile_number,email_id,address,city,state,country,pin_code,date_of_birth FROM user_details Where status = 1 order by id LIMIT 5 OFFSET 0 "
+    # Query1 = "SELECT id,first_name,last_name,mobile_number,email_id,address,city,state,country,pin_code,date_of_birth FROM user_details LIMIT 15, 10"
     try:
         CreateConnection.cursor.execute(Query1)
         data = CreateConnection.cursor.fetchall()
-        i = 0
+        # i = 0
         for x in data:
-            if(i>=row):
-                i=0
-                break
+            # if(i>=row):
+            #     i=0
+            #     break
             print(x)
-            i=i+1
+            # i=i+1
         print("Successful")
     except Exception as e: 
         print(e)
