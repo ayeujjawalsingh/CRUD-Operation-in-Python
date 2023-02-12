dca9e227fc92133b39f3e1ae12f30509646c8518
Ujjawal Singh <ayeujjawalsingh@gmail.com>
2023-02-12 21:17:40 +0530

    Complete

--- ReadData.py
+++ ReadData.py
@@ -1,6 +1,6 @@
 import CreateConnection
 def read():
-    # row = int(input("How much number of row you want to print : "))
+    # row = int(input("How much number of rows you want to print : "))
     # Query1 = "Select id,first_name,last_name,mobile_number,email_id,address,city,state,country,pin_code,date_of_birth From user_details Where status = 1"
     # Query1 = "SELECT id,first_name,last_name,mobile_number,email_id,address,city,state,country,pin_code,date_of_birth FROM user_details order by id LIMIT 30"
     Query1 = "SELECT id,first_name,last_name,mobile_number,email_id,address,city,state,country,pin_code,date_of_birth FROM user_details Where status = 1 order by id LIMIT 5 OFFSET 0 "
