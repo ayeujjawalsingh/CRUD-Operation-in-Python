f8904b19621b3c3c1ca0818ddf55913ded03a8f1
Ujjawal Singh <ayeujjawalsingh@gmail.com>
2023-02-12 21:23:23 +0530

    Complete

--- ShowTables.py
+++ ShowTables.py
@@ -1,7 +1,7 @@
 import CreateConnection
 
 def ShowTables():
-    CreateConnection.cursor.execute("SHOW TABLES")
+    CreateConnection.cursor.execute("SELECT table_name FROM information_schema.tables WHERE table_schema = 'public'")
     for x in CreateConnection.cursor:
         print(x)
 
